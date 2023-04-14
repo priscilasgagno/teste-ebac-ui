2556781455ec3ab734d5813195fc5c0fc3979942
Priscila Gagno <priscilasgagno@gmail.com>
2023-04-14 11:02:57 -0300

    Uso de banco de dados

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,30 +1,49 @@
 /// <reference types="cypress" />
-context ('Funcionalidade Login', () => {
+const perfil = require('../fixtures/perfil.json')
+
+context('Funcionalidade Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')  
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
-    it ('deve fazer login com sucesso', () => {
+    it('deve fazer login com sucesso', () => {
 
-cy.get('#username').type('aluno_ebac@teste.com')
-cy.get('#password').type('teste@teste.com')
-cy.get('.woocommerce-form > .button').click()
-cy.get('#main > .woocommerce').should('contain', 'Pedidos')
-    })
-
-    it ('deve exibir uma mensagem de erro ao inserir usuario invalido' , () => {
-       
-        cy.get('#username').type('aoupotr@teste.com')
-cy.get('#password').type('teste@teste.com')
-cy.get('.woocommerce-form > .button').click()
-cy.get('.woocommerce-error > li').should('contain', 'desconhecido')
-    })
-
-    it ('deve exibir uma mensagem de erro ao inserir senha invalida' , () => {
-      
         cy.get('#username').type('aluno_ebac@teste.com')
-cy.get('#password').type('teste@teste')
-cy.get('.woocommerce-form > .button').click()
-cy.get('.woocommerce-error > li').should('contain', 'incorreta')
-    })   
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('#main > .woocommerce').should('contain', 'Pedidos')
+    })
+
+    it('deve fazer login com sucesso usando base de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('#main > .woocommerce').should('contain', 'Pedidos')
+
+    });
+
+    it('deve fazer login com sucesso usando a fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+            cy.get('#main > .woocommerce').should('contain', 'Pedidos')    
+        })
+    });
+
+    it('deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
+
+        cy.get('#username').type('aoupotr@teste.com')
+        cy.get('#password').type('teste@teste.com')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'desconhecido')
+    })
+
+    it('deve exibir uma mensagem de erro ao inserir senha invalida', () => {
+
+        cy.get('#username').type('aluno_ebac@teste.com')
+        cy.get('#password').type('teste@teste')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-error > li').should('contain', 'incorreta')
+    })
 })
